ccc3663318684cf7639614e8de4df964e960966d
jmoraguiard <jmoraguiard@85986347-2b25-1665-1aa2-4c5569d7f9d2>
2012-11-19 08:56:02 +0000

    Fent proves amb codi de cplusplus.com de sstream :)

--- trunk/game-engine-architecture-tut0/game-engine-architecture-tut0.cpp
+++ trunk/game-engine-architecture-tut0/game-engine-architecture-tut0.cpp
@@ -1,11 +1,24 @@
 // game-engine-architecture-tut0.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
 
-
-int _tmain(int argc, _TCHAR* argv[])
+int main ()
 {
-	return 0;
+  string mystr;
+  float price=0;
+  int quantity=0;
+
+  cout << "Enter price: ";
+  getline (cin,mystr);
+  stringstream(mystr) >> price;
+  cout << "Enter quantity: ";
+  getline (cin,mystr);
+  stringstream(mystr) >> quantity;
+  cout << "Total price: " << price*quantity << endl;
+  getline (cin, mystr);
 }
 
