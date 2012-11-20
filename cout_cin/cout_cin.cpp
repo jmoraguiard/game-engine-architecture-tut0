69cb320573cffe8f1ae91e467c5130f6c6125767
jmoraguiard <jmoraguiard@85986347-2b25-1665-1aa2-4c5569d7f9d2>
2012-11-20 07:10:17 +0000

    Adding projects for cplusplus tutorials in different projects.

---
+++ cout_cin/cout_cin.cpp
@@ -0,0 +1,29 @@
+// game-engine-architecture-tut0.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main ()
+{
+  string mystr;
+
+  float price=0;
+  int quantity=0;
+
+  // COUT AND CIN EXAMPLES
+  cout << "Enter price: ";
+  getline (cin,mystr);
+  stringstream(mystr) >> price;
+  cout << "Enter quantity: ";
+  getline (cin,mystr);
+  stringstream(mystr) >> quantity;
+  cout << "Total price: " << price*quantity << endl;
+
+  getline (cin, mystr);
+
+  return (0);
+}
+
--- game-engine-architecture-tut0/game-engine-architecture-tut0.cpp
+++ game-engine-architecture-tut0/game-engine-architecture-tut0.cpp
@@ -1,6 +1,3 @@
-// game-engine-architecture-tut0.cpp : Defines the entry point for the console application.
-//
-
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -9,16 +6,11 @@ using namespace std;
 int main ()
 {
   string mystr;
-  float price=0;
-  int quantity=0;
 
-  cout << "Enter price: ";
-  getline (cin,mystr);
-  stringstream(mystr) >> price;
-  cout << "Enter quantity: ";
-  getline (cin,mystr);
-  stringstream(mystr) >> quantity;
-  cout << "Total price: " << price*quantity << endl;
+  
+
   getline (cin, mystr);
+
+  return (0);
 }
 
