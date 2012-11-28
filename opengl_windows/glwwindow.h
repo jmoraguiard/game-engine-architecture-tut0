2e02ac0b6f53b285716ab9afaef50064058a5a6d
jmoraguiard@gmail.com <jmoraguiard@gmail.com@85986347-2b25-1665-1aa2-4c5569d7f9d2>
2012-11-28 16:15:36 +0000

    Adding OpenGL on Windows from scratch project.

---
+++ opengl_windows/glwwindow.h
@@ -0,0 +1,33 @@
+#ifndef _GLWINDOW_H
+#define _GLWINDOW_H
+
+#include <windows.h>
+
+class Example; //Declare our Example class
+
+class GLWindow 
+{
+		public:
+				GLWindow(HINSTANCE instance_handler); //default constructor
+				
+				bool create();
+				void destroy();
+				void processEvents();
+				void attachExample(Example* example);
+    
+				bool isRunning(); //Is the window running?
+
+				static LRESULT CALLBACK StaticWindowProcess(HWND window_handler, UINT message_code, WPARAM w_additional_data, LPARAM l_additional_data);
+				LRESULT CALLBACK WindowProcess(HWND window_handler, UINT message_code, WPARAM w_additional_data, LPARAM l_additional_data);
+
+		private:
+				void OnResize(HWND window_handler, UINT flag, int width, int height);
+
+				bool is_running_;
+
+				HWND window_handler_;
+				HINSTANCE instance_handler_;
+				WNDCLASSEX window_class; //Contains window class information. (http://msdn.microsoft.com/en-us/library/windows/desktop/ms633577%28v=vs.85%29.aspx)
+};
+
+#endif
---
+++ opengl_windows/main.cpp
@@ -0,0 +1,29 @@
+#ifdef _WIN32
+#include <windows.h>
+#include "glwwindow.h"
+#endif
+
+//wWinMain is the conventional and most recent name used for the application entry point. (http://msdn.microsoft.com/en-us/library/windows/desktop/ff381406%28v=vs.85%29.aspx)
+#ifdef _WIN32
+int WINAPI wWinMain(HINSTANCE instance_handler, //instance_handler is something called a "handle to an instance" or "handle to a module." The operating system uses this value to identify the executable (EXE) when it is loaded in memory. The instance handle is needed for certain Windows functions — for example, to load icons or bitmaps.
+										HINSTANCE instance_previous_handler, //instance_previous_handler has no meaning. It was used in 16-bit Windows, but is now always zero.
+										PWSTR pCmdLine, //pCmdLine contains the command-line arguments as a Unicode string.
+										int nCmdShow) { //nCmdShow is a flag that says whether the main application window will be minimized, maximized, or shown normally.
+#endif
+
+		GLWindow program_window(instance_handler);
+
+		if(!program_window.create()){
+				MessageBox(NULL, "Window Creation Failed!", "An error ocurrer", MB_ICONINFORMATION | MB_OK);
+				return 0;
+		}
+
+		while(program_window.isRunning())
+    {
+        program_window.processEvents(); //Process any window events
+    }
+
+    program_window.destroy(); //Destroy the program window
+
+		return 0; //The return value is not used by the operating system, but you can use the return value to convey a status code to some other program that you write.
+}
